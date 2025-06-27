c2c1d7f32e1a1212215d049a4356be61bf344b05
Rejinrajeev <rejn.raj.rajeev@gmail.com>
2025-06-27 15:14:28 +0530

    some changes

--- mul_5.js
+++ mul_5.js
@@ -1,3 +1,5 @@
 
 for(let i=0;i<=10;i++){
     console.log(i*5)}
+// This code prints the first 11 multiples of 5, from 0 to 50.
+// It uses a for loop that iterates from 0 to 10, multiplying the loop
